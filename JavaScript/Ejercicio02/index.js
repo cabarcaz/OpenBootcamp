83472ebfac14d1e5ce99335850bbe4abfb9cfb16
Camilo Abarca <cabarcaz@yahoo.cl>
2022-05-18 18:07:40 -0400

    Ejercicio 02

---
+++ JavaScript/Ejercicio02/index.js
@@ -0,0 +1,3 @@
+
+
+var nuevaLista = ["Camilo", 23, true, new Date(1989,10,18), {titulo: "El señor de los anillos", autor: "J.R.R. Tolkien", fecha: new Date(2010, 11, 12), url: "https://www.google.com/"}];
