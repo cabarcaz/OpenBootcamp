e5cf6a1a86ca7547b0164863a8b8c2d94ef6017d
Camilo Abarca <cabarcaz@yahoo.cl>
2022-07-16 15:30:55 -0400

    Ejercicio 05

---
+++ JavaScript/05-Numbers/index.js
@@ -0,0 +1,8 @@
+const alturaCm = 171;
+const alturaM = 1.71;
+const pesoKg = 70.2
+
+const altura = Math.ceil(alturaM);
+const peso = Math.floor(pesoKg);
+
+const iguales = Number.Max_VALUE + 1 === Number.MAX_VALUE;
